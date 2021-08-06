d94bf704801f5ec760f7d4331cbd00c5350aab44
Jakub Gawecki <jmgawecki@icloud.com>
2021-08-06 21:39:19 +0100

    Initial Commit

---
+++ C++Playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++Playground
+//
+//  Created by Jakub Gawecki on 06/08/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
